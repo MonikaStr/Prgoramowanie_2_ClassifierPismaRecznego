fed468683453501b0b6224daabb4151878ab5eae
Monika Mazur <monika.mazurstr@gmail.com>
2018-07-22 14:46:50 +0200

    creating list of arrays of strings

--- src/Main/Classifier.java
+++ src/Main/Classifier.java
@@ -5,7 +5,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Classifier {
 
@@ -20,16 +22,15 @@ public class Classifier {
             System.out.println("Nie znaleziono pliku");
         }
 
-        System.out.println(listOfPixels.get(0));
-        System.out.println(listOfPixels.get(1));
-        System.out.println(listOfPixels.get(2));
-        System.out.println(listOfPixels.get(3));
-        System.out.println(listOfPixels.get(9));
+        List<String[]> listOfLines = listOfPixels
+                .stream()
+                .map(x -> x.split(","))
+                .collect(Collectors.toList());
+
+        System.out.println(Arrays.toString(listOfLines.get(0)));
+
 
     }
 }
 
-/*
-java.nio.file Files.readAllLines -> przeczyta plik i zwróci nam List<string> dla każdej lini/wiersza w pliku
-    java.nio.file Paths.get
- */
+
